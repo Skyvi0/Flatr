53f1e7093658bdea5ecc6ece8af7ce9cfedd3db4
vitaliy <vitaliy.skoryy@googlemail.com>
2022-05-24 17:57:07 +0200

    fixed pathing bug in sheema

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_130619) do
+ActiveRecord::Schema.define(version: 2022_05_24_155100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "flats", force: :cascade do |t|
-    t.string "user_id"
     t.string "adress"
     t.string "description"
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +39,5 @@ ActiveRecord::Schema.define(version: 2022_05_24_130619) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "flats", "users"
 end

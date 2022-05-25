fe2fe32054dfe0a4fde7a7e595ad289650c9924e
vitaliy <vitaliy.skoryy@googlemail.com>
2022-05-25 15:20:26 +0200

    seeds working

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
-User.create(email: "test@test.net", first_name: "bob", last_name: "hogen")
+User.create(email: "test@test.net", first_name: "bob", last_name: "hogen", password: "AaBb14421")
 
 Flat.destroy_all
 8.times do
-  flat = Flat.new(
+  Flat.create(
+    user: User.last,
     adress: "443 South Manor Ave. Palos Verdes Peninsula, CA 90274",
     description: "Peninsula Street",
     price: 456
   )
-  flat.user = User.last
   puts "flat created"
 end

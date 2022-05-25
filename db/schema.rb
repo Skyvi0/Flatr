5eb2908c001865fe0fd763086499152859533db8
vitaliy <vitaliy.skoryy@googlemail.com>
2022-05-25 12:48:11 +0200

    old feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_155100) do
+ActiveRecord::Schema.define(version: 2022_05_25_104050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "flat_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "user_id"
+  end
 
   create_table "flats", force: :cascade do |t|
     t.string "adress"
